a6bd0b54d506217eda34dfbb8630c511bcabdea2
Gowtham-M-Kumar <gowthammkumar2025@gmail.com>
2025-08-03 13:46:52 +0530

    Fix ALLOWED_HOSTS to include Railway domains

--- portfolio_project/portfolio_project/settings.py
+++ portfolio_project/portfolio_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-fpgd5#*72=jc3qjf(1ywb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,.onrender.com', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,.onrender.com,.railway.app', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
